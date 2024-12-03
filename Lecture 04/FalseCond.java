17844b3f9baae1198117e920a449bf6de2f4a331
Priyanshu-Singh-Kushwaha <143744329+Priyanshu-Singh-Kushwaha@users.noreply.github.com>
2024-12-03 12:07:21 +0530

    Code Lecture 4

---
+++ Lecture 04/FalseCond.java
@@ -0,0 +1,11 @@
+public class FalseCond {
+    public static void main(String[] args) {
+        int i = 12;
+        while(i<11){
+            System.out.println("Priyanshu");
+        }
+        do {
+            System.out.println("Priyanshu");
+        } while (i<11);
+    }
+}
---
+++ Lecture 04/LoopHello.java
@@ -0,0 +1,6 @@
+public class LoopHello{
+    public static void main(String[] args){
+        for(int counter=0 ; counter<100 ; counter++)
+        System.out.println("Hello World");
+    }
+}
